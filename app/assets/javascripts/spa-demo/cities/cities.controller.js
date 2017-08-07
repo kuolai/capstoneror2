f2636401adb45b8c830019a662e84fc1025252c0
kuolai <kuolaisha@gmail.com>
2017-08-07 14:32:43 -0700

    capstone mod2assignment before listing resources

---
+++ app/assets/javascripts/spa-demo/cities/cities.controller.js
@@ -0,0 +1,31 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('spa-demo.cities')
+        .controller('spa-demo.cities.CitiesController', CitiesController);
+
+    CitiesController.$inject = ['spa-demo.cities.City'];
+
+    /* @ngInject */
+    function CitiesController(City) {
+        var vm = this;
+        vm.cities;
+        vm.city;
+
+        activate();
+        return;
+
+        ////////////////
+
+        function activate() {
+        	newCity();
+        }
+        function newCity() {
+        	vm.city = new City();
+        }
+        function handleError(response) {
+        	console.log(response);
+        }
+    }
+})();
